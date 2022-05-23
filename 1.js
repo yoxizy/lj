86e2f59102a68fb0680ac0993f7d8a78c5070ded
yoxizy <30953124+yoxizy@users.noreply.github.com>
2022-05-23 09:46:03 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,2 +1,2 @@
-body = $response.body.replace(/6500.00/g, '16500.00').replace(/346197.44/g, '356197.44').replace(/17902.56/g, '7902.56').replace(/13198.38/g, '3198.38');
+body = $response.body.replace(/26971.00/g, '76971.00').replace(/574867.77/g, '624867.77').replace(/70009.23/g, '20009.23').replace(/61704.04/g, '11704.04');
 $done({body});
